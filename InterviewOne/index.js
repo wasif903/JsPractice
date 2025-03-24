bc978aa60895a882afe211fa713c0ddb7df56b4d
wasif903 <wasifmehmood903@gmail.com>
2025-03-25 01:56:46 +0500

    change

---
+++ InterviewOne/index.js
@@ -0,0 +1,47 @@
+// What Are Template Literals ?
+// Template literals are string literals allowing embedded expressions. You can use multi-line strings and string interpolation features with them. They were called "template strings" in prior editions of the ES2015 specification.
+
+// Example:
+// const name = "Wasif";
+// const age = 24;
+// console.log(`My name is ${name} and my age is ${age}`);
+
+// ----------------------------------------------------------
+
+// What are pure and impure functions ?
+// Pure Function: If the function doesn't relies on or modifies external state, Always returns the same output for the same input, it becomes pure.
+
+// Impure Function: the function relies on or modifies external state, it becomes impure
+
+// ----------------------------------------------------------
+
+// Sorting For Alphabetical Order
+// Ascending Order
+// const arr = ["A", "C", "E", "B"];
+// arr.sort();
+// console.log(arr)
+
+// Descending Order
+// const arr = ["A", "C", "E", "B"];
+// arr.sort((a, b) => b.localeCompare(a));
+// console.log(arr)
+
+
+// ----------------------------------------------------------
+
+// const arr = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+//   [7, 8],
+//   [9, 10],
+// ];
+// console.log(arr.length);
+
+
+// ----------------------------------------------------------
+
+
+// It will be considered as [undefined, undefined, undefined,undefined ]
+// const arr1 = [, , ,];
+// console.log(arr1[0]);
