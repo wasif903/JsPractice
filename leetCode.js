c24008fc82da1be2bb6c737eb7816b007c20a982
hasan8733 <imtiazhasan873@gmail.com>
2025-03-24 21:31:56 +0500

    push

--- leetCode.js
+++ leetCode.js
@@ -93,3 +93,51 @@
 // l2.next.next = new ListNode(4);
 
 // console.log(addTwoNumbers(l1, l2));
+
+
+// ----------------------------------- Question 3 ------------------------------
+// ----------------------------------- Longest Substring Without Repeating Characters ------------------------------
+
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+// const lengthOfLongestSubstring = (s) => {
+//     let newString = "";
+//     let largest = 0;
+
+//     for (let i = 0; i < s.length; i++) {
+//         const char = s.charAt(i);
+
+//         if (newString.includes(char)) {
+//             newString = newString.slice(newString.indexOf(char) + 1);
+//         }
+
+//         newString += char;
+
+//         if (newString.length > largest) {
+//             largest = newString.length;
+//         }
+//     }
+
+//     return largest;
+// };
+
+// console.log(lengthOfLongestSubstring("abcabcbb"));
+// console.log(lengthOfLongestSubstring("bbbbb"));
+// console.log(lengthOfLongestSubstring("pwwkew"));
