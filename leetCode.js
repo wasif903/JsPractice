4a1e510ccce078e98c0df294888f52881dfb3424
hasan8733 <imtiazhasan873@gmail.com>
2025-03-24 21:37:46 +0500

    push

--- leetCode.js
+++ leetCode.js
@@ -141,3 +141,37 @@
 // console.log(lengthOfLongestSubstring("abcabcbb"));
 // console.log(lengthOfLongestSubstring("bbbbb"));
 // console.log(lengthOfLongestSubstring("pwwkew"));
+
+// ----------------------------------- Question 4 ------------------------------
+// ----------------------------------- Median of Two Sorted Arrays ------------------------------
+
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+// The overall run time complexity should be O(log (m+n)).
+
+// Example 1:
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+
+// Example 2:
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+// const findMedianSortedArrays = (nums1, nums2) => {
+//     let newArray = [...nums1, ...nums2];
+//     let median = 0;
+
+//     newArray = newArray.sort((a, b) => (a - b));
+
+//     if (newArray.length % 2 == 0) {
+//         median = (newArray[Math.ceil(newArray.length / 2) - 1] + newArray[Math.ceil(newArray.length / 2)]) / 2;
+//     } else {
+//         median = newArray[Math.ceil(newArray.length / 2) - 1];
+//     }
+
+//     return median;
+// };
+
+// console.log(findMedianSortedArrays([1, 3], [2]));
+// console.log(findMedianSortedArrays([1, 2], [3, 4]));
