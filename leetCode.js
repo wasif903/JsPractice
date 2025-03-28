d7dc609e166de89f74a1544a6e7d9005283ae708
hasan8733 <imtiazhasan873@gmail.com>
2025-03-28 22:01:30 +0500

    push

--- leetCode.js
+++ leetCode.js
@@ -218,3 +218,55 @@
 
 // console.log(longestPalindrome("babad"));
 // console.log(longestPalindrome("cbbd"));
+
+
+// ----------------------------------- Question 6 ------------------------------
+// ----------------------------------- Zigzag Conversion ------------------------------
+
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+// Write the code that will take a string and make this conversion given a number of rows:
+// string convert(string s, int numRows);
+
+
+// Example 1:
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+
+// Example 2:
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+
+// Example 3:
+// Input: s = "A", numRows = 1
+// Output: "A"
+
+// const convert = (s, numRows) => {
+//     if (numRows === 1) return s;
+
+//     const rows = new Array(numRows).fill("");
+//     let currentRow = 0;
+//     let goingDown = false;
+
+//     for (const char of s) {
+//         rows[currentRow] += char;
+//         if (currentRow === 0 || currentRow === numRows - 1) {
+//             goingDown = !goingDown;
+//         }
+//         currentRow += goingDown ? 1 : -1;
+//     }
+
+//     return rows.join("");
+// };
+
+// console.log(convert("PAYPALISHIRING", 3));
+// console.log(convert("PAYPALISHIRING", 4));
+// console.log(convert("A", 1));
